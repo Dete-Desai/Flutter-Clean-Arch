d90b667ece01e0bac6b815233f239a93d1eb9472
TheRickFred <naest2473@gmail.com>
2024-01-16 19:37:20 +0300

    updates on bloc and cubit

---
+++ clean_arch/lib/src/authentication/presentation/bloc/authentication_state.dart
@@ -0,0 +1,43 @@
+import 'package:clean_arch/src/authentication/domain/entities/user.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class AuthenticationState extends Equatable {
+  const AuthenticationState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AuthenticationInitial extends AuthenticationState {
+  const AuthenticationInitial();
+}
+
+class CreatingUser extends AuthenticationState {
+  const CreatingUser();
+}
+
+class GettingUsers extends AuthenticationState {
+  const GettingUsers();
+}
+
+class UserCreated extends AuthenticationState {
+  const UserCreated();
+}
+
+class UsersLoaded extends AuthenticationState {
+  const UsersLoaded(this.users);
+
+  final List<User> users;
+
+  @override
+  List<Object> get props => users.map((user) => user.id).toList();
+}
+
+class AuthenticationError extends AuthenticationState {
+  const AuthenticationError(this.message);
+
+  final String message;
+
+  @override
+  List<String> get props => [message];
+}
